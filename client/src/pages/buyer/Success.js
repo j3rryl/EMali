e3473683f6f48a916caed45bed23e301901a1dc6
j3rryl <jeremymunroe0@gmail.com>
2023-01-23 23:54:40 +0300

    images..

---
+++ client/src/pages/buyer/Success.js
@@ -0,0 +1,14 @@
+import { ToastContainer } from "react-toastify";
+
+const Success = () => {
+    return (
+      <div>
+        <h1>Success</h1>
+        <h2>Thank you for your purchase!</h2>
+      </div>
+      
+    );
+  };
+  
+  export default Success;
+  
